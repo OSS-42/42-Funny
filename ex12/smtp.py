a77e365c8273c2987c06e43a3f1f737ee227f199
xDeadpoolx <eric.wursteisen@gmail.com>
2023-04-03 11:17:11 -0400

    fixed ex12

--- ex12/smtp.py
+++ ex12/smtp.py
@@ -7,25 +7,26 @@ from email.mime.base import MIMEBase
 from email.mime.text import MIMEText
 from email.utils import COMMASPACE
 from email import encoders
+from email.mime.application import MIMEApplication
 
 # Read SMTP password from file
 with open("credentials.txt", "r") as f:
-    smtp_password = f.readline().strip()
+	smtp_password = f.readline().strip()
 
 # SMTP connection details
-smtp_server = "mailslurp.mx"
-smtp_port = 2587
-smtp_username = "hHQx4KJgaURh4szXPg8cbqn0sr7nLnyb"
+smtp_server = "smtp-mail.outlook.com"
+smtp_port = 25
+smtp_username = "Aprilsfooltest@outlook.com"
 
 # Email details
 # from_email = "addf6f46-a7fb-46ce-a5c5-2a21559aa298@mailslurp.mx"
-from_email = "c1d61330-8ac1-401f-a23e-cdf970ce6747@mailslurp.com"
-to_email = "c1d61330-8ac1-401f-a23e-cdf970ce6747@mailslurp.com"
-subject = "Test email with attachment #10"
+from_email = "Aprilsfooltest@outlook.com"
+to_email = "Aprilsfooltest@outlook.com"
+subject = "Test email with attachment #15"
 body = "This is a test email with an attachment."
 
 # Create the email with attachment
-msg = MIMEMultipart("html")
+msg = MIMEMultipart()
 msg["From"] = from_email
 msg["To"] = COMMASPACE.join([to_email])
 msg["Subject"] = subject
@@ -33,18 +34,15 @@ msg["Subject"] = subject
 # Add email body
 msg.attach(MIMEText(body, "plain"))
 
-
+# Make the attachment
 attachment_path = "installation.txt"
 filename = os.path.basename(attachment_path)
 with open(attachment_path, "rb") as attachment:
-    part = MIMEBase("application", "octet-stream")
-    part.set_payload(attachment.read())
+	part = MIMEApplication(attachment.read(), _subtype='txt')
 
-# Encode the attachment
-encoders.encode_base64(part)
 
 # Add header as key/value pair to attachment part
-part.add_header("Content-Disposition", "attachment; filename= %s" %filename)
+part.add_header('Content-Disposition', 'part', filename=filename)
 
 # Add attachment to message and convert message to string
 msg.attach(part)
@@ -52,10 +50,10 @@ text = msg.as_string()
 
 # Send the email
 try:
-    with smtplib.SMTP(smtp_server, smtp_port) as server:
-        server.starttls()
-        server.login(smtp_username, smtp_password)
-        server.sendmail(from_email, [to_email], text)
-        print("Email sent successfully.")
+	with smtplib.SMTP(smtp_server, smtp_port) as server:
+		server.starttls()
+		server.login(smtp_username, smtp_password)
+		server.sendmail(from_email, [to_email], text)
+		print("Email sent successfully.")
 except Exception as e:
-    print(f"An error occurred while sending the email: {e}")
+	print(f"An error occurred while sending the email: {e}")
